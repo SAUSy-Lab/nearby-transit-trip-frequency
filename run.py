b58b651f51cc773d62ee5fa0aced3785309178ce
Jeff Allen <jamaps@users.noreply.github.com>
2018-08-02 21:34:08 -0400

    Update run.py

--- run.py
+++ run.py
@@ -7,19 +7,32 @@ start_time = time.time()
 
 # inputs of gtfs and day to count number of trips
 gtfsin = "gtfs" # unzipped folder name
-dayin = "20170322"
+dayin = "20180808"
+
+# # downloading OSM from extensts
+# dl_osm_from_extents(-79.1087,-79.6457,43.8554,43.5807)
 
 # # setting up or killing OSRM server
 # setup_osrm_local("foot")
 # kill_server()
 
+
+# load stops
+dfp_trips = load_trips(gtfsin,dayin)
+dfp_stop_times = load_stop_times(gtfsin)
+print time.time() - start_time
+
 # # running the script for a single origin
-# transitstopsnearby =  stops_distance_away(gtfsin,-79.11551,43.81914,800)
-# print trip_count_day(transitstopsnearby,gtfsin,dayin)
+# transitstopsnearby =  stops_distance_away(gtfsin,-79.12851297,43.79407785,800)
+# print transitstopsnearby
+# tcd = trip_count_day(dfp_stop_times, transitstopsnearby, dfp_trips)
+# print tcd
+
+# trip_count_day(transitstopsnearby,gtfsin,dayin)
 
 # running for a set of origins
-input_data = "geocoded.csv"
-out_data = [["ID","24hr_trip_count"]]
+input_data = "tor_block_pts.csv"
+out_data = [["DBUID","24hr_trip_count"]]
 with open(input_data,"r") as csvfile:
     reader = csv.DictReader(csvfile)
     c = 0
@@ -29,19 +42,17 @@ with open(input_data,"r") as csvfile:
             ox = float(row["X"])
             oy = float(row["Y"])
             transitstopsnearby =  stops_distance_away(gtfsin,ox,oy,800)
-            tcd = trip_count_day(transitstopsnearby,gtfsin,dayin)
-            out_row = ([row["ID"],tcd])
+            tcd = trip_count_day(dfp_stop_times, transitstopsnearby, dfp_trips)
+            out_row = ([row["DBUID"],tcd])
             out_data.append(out_row)
         except:
-            out_row = ([row["ID"],0])
+            out_row = ([row["DBUID"],0])
             out_data.append(out_row)
-        print c
-        print "--------------"
-with open("t_GO_person.csv", "w") as csvfile:
+        print c, "   ", time.time() - start_time
+        print "------------------------------"
+with open("out_data.csv", "w") as csvfile:
     writer = csv.writer(csvfile)
     for row in out_data:
         writer.writerow(row)
 
 print time.time() - start_time
-
-# nearby-transit-trip-frequency
